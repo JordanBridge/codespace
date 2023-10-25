f3d7c4b5bb40a064c6df6fff3eeda599f9b0195d
Jordan Bridge <114111506+JordanBridge@users.noreply.github.com>
2023-10-25 20:52:57 +0000

    Wed, Oct 25, 2023, 8:52 PM +00:00

--- function.c
+++ function.c
@@ -6,11 +6,21 @@ int valid_triangle(int a, int b, int c);
 
 int main(void)
 {
-    valid_triangle(a,b,c);
+    int a,b, c;
 
     a = get_int("Please enter in the value of side 1: \n");
     b = get_int("Please enter in the value of side 2: \n");
     c = get_int("Please enter in the value of side 3: \n");
+
+    if valid_triangle(a,b,c)
+    {
+        printf("This is a valid triangle");
+    }
+
+    else
+    {
+        printf("This triangle is wrong!");
+    }
 
 }
 
@@ -21,10 +31,9 @@ int valid_triangle(int a, int b, int c)
    if (a > 0 && b > 0 && c > 0)
     {
 
-        lengthchecker = a + b > c;
-
-        printf("This makes a valid traingle");
-        return lengthchecker;
+        a + b > c;
+        
+        return 1;
     }
     else
     {
