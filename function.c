22e35a8d654982bb56bfba8655d7495467ead23e
Jordan Bridge <114111506+JordanBridge@users.noreply.github.com>
2023-10-25 20:44:27 +0000

    Wed, Oct 25, 2023, 8:44 PM +00:00

--- function.c
+++ function.c
@@ -7,12 +7,10 @@ int valid_triangle(int a, int b, int c);
 int main(void)
 {
     valid_triangle(a,b,c);
-    
+
     a = get_int("Please enter in the value of side 1: \n");
     b = get_int("Please enter in the value of side 2: \n");
     c = get_int("Please enter in the value of side 3: \n");
-
-
 
 }
 
