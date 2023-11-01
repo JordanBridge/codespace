1070908da28176d59768ce3ee960c6535a8c989f
Jordan Bridge <114111506+JordanBridge@users.noreply.github.com>
2023-11-01 20:04:47 +0000

    Wed, Nov 1, 2023, 8:04 PM +00:00

--- factorial.c
+++ factorial.c
@@ -1,21 +1,3 @@
 #include<stdio.h>
 #include<cs50.h>
 
-int factorial(int number);
-
-int main(void)
-{
-    //prompt for number
-    int n = get_int("Type a number: ");
-    printf("%i\n", factorial(n));
-}
-
-int factorial(int number)
-{
-    if(number == 1)
-    {
-        return 1;
-    }
-
-    return number * factorial(number - 1);
-}
