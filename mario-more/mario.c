00bb60921b24353b9aa020c96674ba1682a6540d
Jordan Bridge <114111506+JordanBridge@users.noreply.github.com>
2023-11-09 20:09:31 +0000

    Thu, Nov 9, 2023, 8:09 PM +00:00

--- mario-more/mario.c
+++ mario-more/mario.c
@@ -4,30 +4,31 @@
 int main(void)
 {
 
-    printf("Welcome To Mario Land, lets make a pryamid today! :D");
+    printf("Welcome To Mario Land, lets make a pryamid today! :D\n");
 
     //get base level
-    plevels = get_int("How many levels do you want your pyramid to have??");
+    plevels = get_int("How many levels do you want your pyramid to have??\n");
 
 
     //validate base level
     if(plevels < 1 )
     {
-        printf("This is an invalid number of levels for your pryamid to be properly built sir!");
+        printf("This is an invalid number of levels for your pryamid to be properly built sir!\n");
         return 0;
     }
 
     else if(plevels > 8)
     {
-        printf("This value is too high!")
+        printf("This value is too high!\n")
         return 0;
     }
 
     else
     {
-        for(plevels; plevels< plevels + 1; plevels++);
-
-        printf("#  #");
+        for(plevels; plevels < plevels + 1; plevels++)
+        {
+            printf("#  # \n");
+        }
 
     }
 
