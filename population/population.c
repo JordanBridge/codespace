7f4c9e83bc729b8596d761bcdb2ff0cdac52f379
Jordan Bridge <114111506+JordanBridge@users.noreply.github.com>
2023-11-09 20:59:40 +0000

    Thu, Nov 9, 2023, 8:59 PM +00:00

--- population/population.c
+++ population/population.c
@@ -5,8 +5,17 @@ int main(void)
 {
     // TODO: Prompt for start size
     do
-
+    {
+       start_size = printf("Please Enter a start size");
+    }
+    while(start_size <= 9);
     // TODO: Prompt for end size
+
+    do
+    {
+        end_size = printf("Please Give Me A Start Size");
+    }
+    while(end_size >= start_size);
 
     // TODO: Calculate number of years until we reach threshold
 
