36d8b37ac96aaeee35d10c827f09e753f3427dce
Jordan Bridge <114111506+JordanBridge@users.noreply.github.com>
2023-11-09 22:07:55 +0000

    Thu, Nov 9, 2023, 10:07 PM +00:00

--- population/population.c
+++ population/population.c
@@ -20,7 +20,7 @@ int main(void)
     }
     while(end_size < start_size);
     // TODO: Calculate number of years until we reach threshold
-    int years = 23;
+    int years = 0;
     while (start_size >= end_size)
     {
         start_size = start_size + (start_size/3) - (end_size/4);
