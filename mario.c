ee564e6c9b21097881b6ee5d4cd82996fa96606f
Jordan Bridge <114111506+JordanBridge@users.noreply.github.com>
2023-11-10 21:49:11 +0000

    Fri, Nov 10, 2023, 9:49 PM +00:00

--- mario.c
+++ mario.c
@@ -1,1 +1,22 @@
-cd
+#include <stdio.h>
+
+int main() {
+    int height = 5; // Adjust the height of the pyramid as needed
+
+    for (int i = 1; i <= height; i++) {
+        // Print spaces before the characters
+        for (int j = 0; j < height - i; j++) {
+            printf(" ");
+        }
+
+        // Print characters for the pyramid
+        for (int k = 0; k < 2 * i - 1; k++) {
+            printf("#");
+        }
+
+        // Move to the next line after printing each row
+        printf("\n");
+    }
+
+    return 0;
+}
