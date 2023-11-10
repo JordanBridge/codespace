12ff3ba3d37f2532ec56c5827afd525764c7ea41
Jordan Bridge <114111506+JordanBridge@users.noreply.github.com>
2023-11-10 23:29:34 +0000

    Fri, Nov 10, 2023, 11:29 PM +00:00

--- mario-less/mario.c
+++ mario-less/mario.c
@@ -10,12 +10,14 @@ int main(void)
     if (ramp > 1)
     {
 
-        for(int row = 1; i < ramp; row++)
+        for(int row = 1; i <= ramp; row++)
         {
-            for(int space = 1; space <=)
+            for(int space = 1; space <= (row - ramp); space++)
             {
                 printf(" ");
             }
+
+            for(int symbol = 1; symbol <= ((2*row)-1; symbol++)
 
 
         }
