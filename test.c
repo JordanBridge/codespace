2caec626e565a1ccff79f1e3e7e7888ea4ea3bba
Jordan Bridge <114111506+JordanBridge@users.noreply.github.com>
2023-11-10 23:43:21 +0000

    Fri, Nov 10, 2023, 11:43 PM +00:00

---
+++ test.c
@@ -0,0 +1,37 @@
+#include <cs50.h>
+#include <stdio.h>
+
+int main(void)
+{
+    printf("Welcome to super mario!\n");
+
+    int ramp = get_int("Please state how long you want your ramp to be: ");
+
+    if (ramp > 1)
+    {
+
+        for(int row = 1; row <= ramp; row++)
+        {
+            for(int space = 1; space <= (row - ramp); space++)
+            {
+                printf(" ");
+            }
+
+            for(int symbol = 1; symbol <= (2*row - 1); symbol++)
+            {
+                printf("#");
+            }
+
+        }
+
+    }
+
+    else
+    {
+        printf("This is one small ramp!\n");
+        printf("\n");
+        printf("#");
+
+    }
+
+}
