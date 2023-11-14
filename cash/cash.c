e4161f5b6a77467df7156863cd642b204c5e8e47
Jordan Bridge <114111506+JordanBridge@users.noreply.github.com>
2023-11-14 20:43:59 +0000

    Tue, Nov 14, 2023, 8:43 PM +00:00

--- cash/cash.c
+++ cash/cash.c
@@ -13,9 +13,9 @@ int main(void)
     int cents = get_cents();
     do
     {
-        get_cents = get_int("How many cents are the customer owed: ");
+        cents = get_int("How many cents are the customer owed: ");
     }
-    while (get_cents > 0);
+    while (cents > 0);
 
     // Calculate the number of quarters to give the customer
     int quarters = calculate_quarters(cents);
