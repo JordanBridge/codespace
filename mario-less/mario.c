9cfc53dfaffc5ce16ca4881d6bdd3afbd302746e
Jordan Bridge <114111506+JordanBridge@users.noreply.github.com>
2023-11-15 19:05:35 +0000

    Wed, Nov 15, 2023, 7:05 PM +00:00

--- mario-less/mario.c
+++ mario-less/mario.c
@@ -3,35 +3,8 @@
 
 int main(void)
 {
-    printf("Welcome to super mario!\n");
-
-    int ramp = get_int("Please state how long you want your ramp to be: ");
-
-    if (ramp > 1)
+    do
     {
-
-        for(int row = 1; row <= ramp; row++)
-        {
-            for(int space = 1; space <= (row - ramp); space++)
-            {
-                printf(" ");
-            }
-
-            for(int symbol = 1; symbol <= (2*row - 1); symbol++)
-            {
-                printf("#");
-            }
-
-        }
-
+        height =get_int("Enter A Height Here: ");
     }
 
-    else
-    {
-        printf("This is one small ramp!\n");
-        printf("\n");
-        printf("#");
-
-    }
-
-}
