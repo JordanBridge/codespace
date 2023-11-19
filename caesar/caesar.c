cd25b5393d16113b73e68dcce5769263f13edf4f
Jordan Bridge <114111506+JordanBridge@users.noreply.github.com>
2023-11-19 21:41:34 +0000

    Sun, Nov 19, 2023, 9:41 PM +00:00

--- caesar/caesar.c
+++ caesar/caesar.c
@@ -7,11 +7,6 @@ int main(int argc, string argv[])
     for(int i = 0; argv[i]!= NULL;i++)
     {
 
-        for(int j; j += 12 && j != NULL; j++)
-        {
-            printf("i", %s);
-        }
-
         printf("Praise Be To Caesar\n");
     }
 
