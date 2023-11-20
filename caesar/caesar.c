80cb4cff1aeec8214fd4db584f2068c7c569236b
Jordan Bridge <114111506+JordanBridge@users.noreply.github.com>
2023-11-20 20:41:43 +0000

    Mon, Nov 20, 2023, 8:41 PM +00:00

--- caesar/caesar.c
+++ caesar/caesar.c
@@ -3,22 +3,6 @@
 
 int main(int argc, string argv[])
 {
-    int n = 3;
 
-    do
-    {
-    for(int i = 0; argv[i];i++)
-    {
-
-       for(int k = 0;k < n; k++)
-       {
-        argv[i]++;
-       }
-
-       printf("%c\n", *argv[i]);
-    }
-    return 0;
-    }
-    while(argc > 1);
 
 }
