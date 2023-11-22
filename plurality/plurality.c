8ab1c29e0cc74ae120ddfef3c460120f09e9c7a2
Jordan Bridge <114111506+JordanBridge@users.noreply.github.com>
2023-11-22 21:00:11 +0000

    Wed, Nov 22, 2023, 9:00 PM +00:00

--- plurality/plurality.c
+++ plurality/plurality.c
@@ -69,7 +69,7 @@ bool vote(string name)
 
     for(int i = 0; candidates[i].name && candidates[i].name != NULL; i++)
     {
-        if((strcmp(name, candidates[i])) == 0)
+        if((strcmp(name, candidates[i].name)) == 0)
         {
             candidates[i].votes = candidates[i + 1].votes;
             return true;
