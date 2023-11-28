9e786bbb3e21b5043de5e4a20d3ae781573583bf
Jordan Bridge <114111506+JordanBridge@users.noreply.github.com>
2023-11-28 20:52:01 +0000

    Tue, Nov 28, 2023, 8:52 PM +00:00

--- runoff/runoff.c
+++ runoff/runoff.c
@@ -130,7 +130,7 @@ bool vote(int voter, int rank, string name)
     // TODO
  for(voter, rank; preferences[voter][rank] != NULL; voter++ rank++)
     {
-        if(strcmp(preferences[voter][],  )
+        if(strcmp(preferences[][rank],  )
         {
 
         }
